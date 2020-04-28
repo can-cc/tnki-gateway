f97054406c56ab43099d8544ba1a46c40d13c3f0
chchen <chchen@thoughtworks.com>
2020-04-28 11:23:13 +0800

    change configure

--- src/main/java/com/tnki/gateway/request/LoginRequest.java
+++ src/main/java/com/tnki/gateway/request/LoginRequest.java
@@ -1,7 +1,5 @@
 package com.tnki.gateway.request;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class LoginRequest {
     private String username;
     private String password;
